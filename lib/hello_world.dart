16e8d28cfe5fe3269116dc3f2981ec2f0558cd23
andihasan97 <andihasanelfalakiy@gmail.com>
2025-04-07 21:21:57 +0700

    first commit

---
+++ lib/hello_world.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  // menampilkan hello world
+  print('Hello, World!');
+
+  // mendeklarasikan value terlebih dahulu sebelum ditampilkan
+  String halo = 'Halo Dunia!';
+  print(halo);
+}
